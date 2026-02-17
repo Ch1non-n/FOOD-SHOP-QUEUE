37f3cdadfcb6b74870d31fa47048edd66b620cd4
Ch1non-n <sornchai.soontranon054@gmail.com>
2026-02-17 12:33:18 -0500

    bug 1

--- main.c
+++ main.c
@@ -17,34 +17,50 @@ int main(int argc, char **argv){
     printf("   1.Sushi 20 baht\n");
     printf("=======================\n");
 
-    int cutomer_count = 0;
+    int profit = 0;
+    int cutomer_count = 1;
     for(int i = 1; i < argc; i++){
         if(strcmp(argv[i], "x") == 0){
-            cutomer_count++;
+            int price = dequeue(&q);
+
+            printf("Cutomer no : %d\n", cutomer_count++);
+            printf("You have to pay : %d\n", price);
+
+            int cash;
+            while(1){
+
+                printf("cash : ");
+                scanf("%d", &cash);
+                if(cash >= price) break;
+                else printf("Insufficient money\n");
+            }
+
+            int change = cash - price;
+            printf("change : %d\n", change);
+            profit += change;
         }
         else{
 
             switch(atoi(argv[i])){
                 case 1: 
-                    enqueue(&q, atoi(argv[i+1]));
+                    enqueue(&q, atoi(argv[i]), atoi(argv[i+1]));
                     printf("My order is : %d\n", atoi(argv[i]));
                     break;
                 case 2:
-                    enqueue(&q, atoi(argv[i+1]));
+                    enqueue(&q, atoi(argv[i]), atoi(argv[i+1]));
                     printf("My order is : %d\n", atoi(argv[i]));
                     break;
                 case 3:
-                    enqueue(&q, atoi(argv[i+1]));
+                    enqueue(&q, atoi(argv[i]), atoi(argv[i+1]));
                     printf("My order is : %d\n", atoi(argv[i]));
                     break;
-                default : printf("No Food");
+                default : printf("No Food\n");
             }
             i++;
             
         }
     }
 
-
-    
+    if(q.size != 0) printf("There are %d people left in the queue", q.size);
     return 0;
 }
--- queue.h
+++ queue.h
@@ -8,14 +8,17 @@ typedef struct queue{
 
 }queue;
 
-void enqueue(queue *q, int quantity);
+void enqueue(queue *q, int order_number, int quantity);
 int dequeue(queue *q);
 
 
-void enqueue(queue *q, int quantity){
+void enqueue(queue *q, int order_number, int quantity){
 
     nodePtr new_order = (nodePtr)malloc(sizeof(NODE));
     if(new_order){
+
+        new_order->order_number = order_number;
+        new_order->quantity = quantity;
 
         new_order->nextPtr = NULL;
         if(q->size == 0) q->headPtr = new_order;
@@ -26,7 +29,28 @@ void enqueue(queue *q, int quantity){
     }
 }
 
+int dequeue(queue *q){
 
+    nodePtr temp = q->headPtr;
+    if(temp){
+        int quantity = q->headPtr->quantity;
+        int order_number = q->headPtr->order_number;
+        int price;
 
+        switch(order_number){
+            case 1: price = 120 * quantity; break;
+            case 2: price = 45 * quantity; break;
+            case 3: price = 20 * quantity; break;
+        }
+
+        q->headPtr = q->headPtr->nextPtr;
+        if(q->size == 1) q->tailPtr = q->tailPtr->nextPtr;
+        free(temp);
+        q->size--;
+        return price;
+    }
+    printf("the queue is empty\n");
+    return 0;
+}
 
 #endif
